a18422a5f62c8c1280fe25dacc1e09d915f7a09b
Ely Karr <elykarr@gmail.com>
2021-09-08 15:54:01 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const main = document.getElementById("main").remove();
+console.log(main)
+
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+
